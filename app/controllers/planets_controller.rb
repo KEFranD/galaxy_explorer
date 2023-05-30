99e43cc4822d8ef71a3c2268c37b9ef3b5e1ab64
Leo DHotman <dhotman19@yahoo.com>
2023-05-30 17:50:36 +0400

    pundit trial

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -2,31 +2,40 @@ class PlanetsController < ApplicationController
   before_action :set_planet, only: %i[show edit update destroy]
 
   def index
-    @planets = Planet.all
+    @planets = policy_scope(Planet)
   end
 
-  def show; end
+  def show
+    authorize @planet
+  end
 
   def new
     @planet = Planet.new
+    authorize @planet
   end
 
   def create
     @planet = Planet.new(planet_params)
+    @planet.user = current_user
+    authorize @planet
     @planet.save
     # No need for app/views/planets/create.html.erb
     redirect_to planet_path(@planet)
   end
 
-  def edit; end
+  def edit
+    authorize @planet
+  end
 
   def update
+    authorize @planet
     @planet.update(planet_params)
     # No need for app/views/planets/update.html.erb
     redirect_to planet_path(@planet)
   end
 
   def destroy
+    authorize @planet
     @planet.destroy
     # No need for app/views/planets/destroy.html.erb
     redirect_to planets_path, status: :see_other
